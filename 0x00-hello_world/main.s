895c22b67dd55a4b445555c53703b399a4501b0d
SyeDanny <syemwayne1122@gmail.com>
2022-09-08 17:11:46 +0300

    Assembly

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,20 +1,27 @@
 	.file	"main.c"
+# GNU C17 (Ubuntu 11.2.0-19ubuntu1) version 11.2.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 11.2.0, GMP version 6.2.1, MPFR version 4.1.0, MPC version 1.2.1, isl version isl-0.24-GMP
+
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -mtune=generic -march=x86-64 -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
+	endbr64	
+	pushq	%rbp	#
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp	#,
 	.cfi_def_cfa_register 6
-	movl	$0, %eax
-	popq	%rbp
+# main.c:10: 	return (0);
+	movl	$0, %eax	#, _1
+# main.c:11: }
+	popq	%rbp	#
 	.cfi_def_cfa 7, 8
-	ret
+	ret	
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main

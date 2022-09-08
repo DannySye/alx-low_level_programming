95af82ca2e686636d1b2340710aab5c4602b0420
SyeDanny <syemwayne1122@gmail.com>
2022-09-08 14:17:04 +0300

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Will print using puts
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char String[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(String);
+	return (0);
+}

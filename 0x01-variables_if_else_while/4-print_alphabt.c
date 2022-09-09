faaffe366804f7b2a6407aec8c7a0e87d338b05e
SyeDanny <syemwayne1122@gmail.com>
2022-09-10 02:16:54 +0300

    qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - This is for the letters
+*
+*Return: 
+*/
+int main(void)
+{
+	char dan[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(dan[i]);
+	}
+	printf("\n");
+	return (0);
+}

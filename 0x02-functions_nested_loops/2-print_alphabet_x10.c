b8c5396420c7709f17c764e694ebbf23434e8039
DannySye <syemwayne1122@gmail.com>
2022-09-15 03:00:29 +0300

    alpabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_alphabet_10x - print alphabet 10x
+*
+*Return: Not always zero
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0 , j;
+
+	while (i < 10 )
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+	_putchar('\n')
+	i++;
+	}
+}

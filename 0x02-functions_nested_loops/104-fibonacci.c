db78b37ecc33a0ed70db915dd66e68bc488c5b94
DannySye <syemwayne1122@gmail.com>
2022-09-15 05:19:59 +0300

    Fibonaci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,6 +10,7 @@ int main(void)
 	int count;
 	unsigned long i, j, k;
 	unsigned long m, n, p;
+
 	carry;
 	count = 0;
 	i = 0;
@@ -30,8 +31,8 @@ int main(void)
 	while (count <= 98)
 	{
 	carry = (m + n) / 1000;
-	p = (m + n) -carry * 1000;
-	k = (i + j) +carry;
+	p = (m + n) - carry * 1000;
+	k = (i + j) + carry;
 	m = n;
 	n = p;
 	i = j;

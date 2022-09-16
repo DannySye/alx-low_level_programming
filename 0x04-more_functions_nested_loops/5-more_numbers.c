9c361e317de5750bd9f8035518832abf498aa388
DannySye <syemwayne1122@gmail.com>
2022-09-17 01:32:16 +0300

    6-lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -21,5 +21,6 @@ void more_numbers(void)
 	_putchar(b % 10 + '0');
 	}
 	_putchar('\n');
+
 }
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+void print_line(int n)
+{
+	int a;
+	while (a <= n)
+	{
+	_putchar(95);
+	a++;
+	}
+	_putchar('\n');
+}

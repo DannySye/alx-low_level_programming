ba6c6e1d61124cdc5287f5d95a3500d3dad99b22
DannySye <syemwayne1122@gmail.com>
2022-09-16 18:38:03 +0300

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,8 +10,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
-	{
-		_putchar(i);
-	}
+	_putchar(i);
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 
-#define MAIN.H
+#define MAIN_H
 
 int _isupper(int c);
 

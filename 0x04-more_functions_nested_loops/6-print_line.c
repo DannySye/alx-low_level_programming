11a52b2703eeb38b6832b020ebdf1b68e0cf3665
DannySye <syemwayne1122@gmail.com>
2022-09-17 01:43:31 +0300

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
-*
-*
-*
+*print_line - test this line
+*@a:
+*@n:int
+*Return: Declared in main
 */
 void print_line(int n)
 {
 	int a;
+
 	while (a <= n)
 	{
 	_putchar(95);

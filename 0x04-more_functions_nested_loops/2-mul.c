fffbd0da1e934ecf4e2523c12052d31eae84be0e
DannySye <syemwayne1122@gmail.com>
2022-09-16 23:23:05 +0300

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
-#include "main"
+#include "main.h"
 
 /**
-*
-*
-*
-*
+*mul - this is to multiply 2 numbers
+*@a: This is the variable a
+*@b: This is the variable b
+*Return: The value of the multiplication
 */
 int mul(int a, int b)
 {

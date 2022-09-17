eb38fc0cd23396f3156dda835cdd00aa86e0b77d
DannySye <syemwayne1122@gmail.com>
2022-09-17 03:13:48 +0300

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
 *print_square - square
-*
+*@size:int
 */
 void print_square(int size)
 {
@@ -10,7 +10,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a =0; a < size; a++)
+		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < size; b++)
 			{

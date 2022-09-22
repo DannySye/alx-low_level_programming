a6977b898a1464b7ddc1dd932188ab0b9384057e
DannySye <syemwayne1122@gmail.com>
2022-09-23 00:54:22 +0300

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*string_toupper - called function
+*@str:for the string
+*Return: str
+*/
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] -32;
+		}
+		i++;
+	}
+	return (str);
+}

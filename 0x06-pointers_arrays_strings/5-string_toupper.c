4dac412a0aa129db4d358cc8656e7a38094cf89a
DannySye <syemwayne1122@gmail.com>
2022-09-23 01:21:02 +0300

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,13 @@
 */
 char *string_toupper(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] -32;
+			str[i] = str[i] - 32;
 		}
 		i++;
 	}

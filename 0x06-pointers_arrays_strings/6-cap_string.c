faf5d104cf5b580802e80850ed12182a30ca8df3
DannySye <syemwayne1122@gmail.com>
2022-09-23 02:23:27 +0300

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*leet - called function
+*@s: called string
+*Return: s
+*/
+char *leet(char *s)
+{
+	int i = 0, j;
+	int lc[] = {97, 101, 111, 116, 108};
+	int cp[] = {65, 69, 79, 84, 76};
+	int m[] = {52, 51, 48, 55, 49};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lc[j] || s[i] == cp[j])
+			{
+				s[i] = m[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}

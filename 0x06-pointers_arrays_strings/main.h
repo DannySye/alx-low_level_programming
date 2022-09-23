9783cb330753f9abe1743431563e56fee33b1b44
DannySye <syemwayne1122@gmail.com>
2022-09-23 23:34:11 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,3 +15,11 @@ char *cap_string(char *);
 char *leet(char *);
 
 int _putchar(char c);
+
+void print_number(int n);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);

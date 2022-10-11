e7e8bd361986dfedd27bfe36f9e86c47a78d2196
DannySye <syemwayne1122@gmail.com>
2022-10-11 03:46:03 +0300

    Outside of a dog, a book is man's best friend. Inside the dog, it's too dark to read

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct type with the following elements
+ * @name: with data type char
+ * @age: with data type int
+ * @owner: with data type char
+ * Description: Structure holding information of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
